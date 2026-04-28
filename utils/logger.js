c014d90164cacf341549338035ca784c7f7297de
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-04-28 12:46:47 +0530

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,7 @@
  * Conditional logging based on environment with performance optimization
  * @module logger
  * @author Mohammad Faiz
- * @version 1.4.0
+ * @version 1.5.0
  */
 
 const IS_PRODUCTION = (() => {
